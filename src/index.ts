a4d85b0ef99b46655a140519192ce40ef23bb525
Alexander Rudolph <alexr30@web.de>
2024-01-08 16:05:03 +0100

    Working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
+import { RichText } from "@atproto/api";
+import { BskyPostRecord } from "./types/bsky";
+import { getMentions, getPost, reply } from "./lib/bsky";
 import { count } from "./lib/counter";
+import { getLastId, setLastId } from "./lib/github";
 
-count("Frank Walter Steinmeier ruft im Stundentakt bei mir an und bettelt um meine boy pussy. Olaf Scholz kann vielleicht seine Fleischpeitsche gut schwingen, aber niemand lässt die bussy so poppen wie ich");
+//count("Frank Walter Steinmeier ruft im Stundentakt bei mir an und bettelt um meine boy pussy. Olaf Scholz kann vielleicht seine Fleischpeitsche gut schwingen, aber niemand lässt die bussy so poppen wie ich");
+
+
+getMentions().then(async (mentions) => {
+	let lastId = await getLastId();
+	for(let mention of mentions) {
+		if(mention.cid === lastId) break;
+
+		let parentUri = (mention.record as BskyPostRecord).reply.parent.uri;
+		let post = await getPost(parentUri);
+		let occurrences = count(post.text);
+		let text = "";
+
+		for(let occurrence of occurrences) {
+			text += `"${occurrence[0]}" ist ${occurrence[1]} mal in der Bibel\n`;
+		}
+		if(text === "") {
+			text = "Keines dieser Wörter kommt in der Bibel vor";
+		}
+
+		let answerText = new RichText({
+			text: text
+		});
+		await reply({
+			parent: {uri: mention.uri, cid: mention.cid},
+			root: {uri: (mention.record as BskyPostRecord).reply.root.uri, cid: (mention.record as BskyPostRecord).reply.root.cid},
+			text: answerText
+		});
+		//console.log((mention.record as BskyPostRecord).reply.parent);
+	}
+
+	await setLastId(mentions[0].cid);
+});

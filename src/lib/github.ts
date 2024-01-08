2195bb65810996566388f622330d3c54a74fdb24
Alexander Rudolph <alexr30@web.de>
2024-01-08 16:03:46 +0100

    Github integration

---
+++ src/lib/github.ts
@@ -0,0 +1,26 @@
+import { Octokit } from "@octokit/core";
+
+const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
+
+export async function getLastId() {
+	return (await octokit.request('GET /repos/{owner}/{repo}/actions/variables/{name}', {
+		owner: "drblaui",
+		repo: "bsky-bible-bot",
+		name: "LAST",
+		header: {
+			'X-GitHub-Api-Version': "2022-11-28"
+		}
+	})).data.value;
+}
+
+export async function setLastId(id: string) {
+	await octokit.request('PATCH /repos/{owner}/{repo}/actions/variables/{name}', {
+		owner: "drblaui",
+		repo: "bsky-bible-bot",
+		name: "LAST",
+		value: id,
+		headers: {
+			'X-GitHub-Api-Version': '2022-11-28'
+		}
+	});
+}

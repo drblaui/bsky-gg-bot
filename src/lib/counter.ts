a9de826affe40595c905bcd582af64302518feaa
Alexander Rudolph <a.rudolph@itc.drk.de>
2024-05-24 16:09:56 +0200

    Update Counter to not remove umlauts

--- src/lib/counter.ts
+++ src/lib/counter.ts
@@ -9,9 +9,9 @@ export function count(str: string) {
 	let res = [];
 
 	for(let word of str.split(" ")) {
-		if(word === "" || word.replace(/\W/g, "") === "") continue;
-		if (bible.includes(word.replace(/\W/g, ""))) {
-			let wordRegexp = new RegExp("\\s" + word.replace(/\W/g, "") + "\\s", "gi");
+		if(word === "" || word.replace(/[^äöüßÄÖÜa-zA-Z0-9]/g, "") === "") continue;
+		if (bible.includes(word.replace(/[^äöüßÄÖÜa-zA-Z0-9]/g, ""))) {
+			let wordRegexp = new RegExp("\\s" + word.replace(/[^äöüßÄÖÜa-zA-Z0-9]/g, "") + "\\s", "gi");
 			let count = (bible.match(wordRegexp) || []).length;
 			if(count <= 0) continue;
 			res.push([word, count])
